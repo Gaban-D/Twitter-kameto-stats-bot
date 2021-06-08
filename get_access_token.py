ece832286a5b0004e218d8b01a70740ecaefd0c8
Gabin Deboulogne <gabin.deboulogne@gmail.com>
2021-06-09 01:31:32 +0200

    Added a script to get twitter account access token

---
+++ get_access_token.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import tweepy
+import os
+
+load_dotenv()
+
+auth = tweepy.OAuthHandler(os.environ.get("twitter_consumer_public"), os.environ.get("twitter_consumer_secret"))
+
+try:
+	redirect_url = auth.get_authorization_url()
+	print(redirect_url)
+except tweepy.TweepError:
+	print('Error! Failed to get request token.')
+	quit()
+
+verifier = input("Enter the code given by twitter website: ")
+
+try:
+	tokens = auth.get_access_token(verifier)
+	pprint(tokens)
+except tweepy.TweepError:
+	print('Error! Failed to get access token.')
+	exit()
